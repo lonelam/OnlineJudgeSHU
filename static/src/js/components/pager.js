3b0f8c73a1df04750d4265252427e7c35ea17090
lonelam <laizenan@gmail.com>
2017-06-19 16:46:52 +0800

    修改了分页js

--- static/src/js/components/pager.js
+++ static/src/js/components/pager.js
@@ -3,13 +3,21 @@ define("pager", ["avalon"], function (avalon) {
     };
     avalon.component("ms:pager", {
         $template: "页数: {{ currentPage }}/{{ totalPage }} " +
+        "<button ms-class=\"{{ currentPage==1?'btn btn-primary disabled':'btn btn-primary' }}\" ms-click=\"_getFirstPage\">首页</button> " +
         "<button ms-class=\"{{ currentPage==1?'btn btn-primary disabled':'btn btn-primary' }}\" ms-click=\"_getPrevPage\">上一页</button> " +
-        " <button ms-class=\"{{ currentPage==totalPage?'btn btn-primary disabled':'btn btn-primary' }}\" ms-click=\"_getNextPage\">下一页</button>",
+        " <button ms-class=\"{{ currentPage==totalPage?'btn btn-primary disabled':'btn btn-primary' }}\" ms-click=\"_getNextPage\">下一页</button>" +
+        " <button ms-class=\"{{ currentPage==totalPage?'btn btn-primary disabled':'btn btn-primary' }}\" ms-click=\"_getLastPage\">尾页</button>",
         currentPage: 1,
         totalPage: 1,
         _getPrevPage: _interface,
         _getNextPage: _interface,
         $init: function (vm, el) {
+            vm._getFirstPage = function () {
+                if (vm.currentPage > 1) {
+
+                    vm.getPage(1);
+                }
+            };
             vm._getPrevPage = function () {
                 if (vm.currentPage > 1) {
                     vm.currentPage--;
@@ -20,6 +28,11 @@ define("pager", ["avalon"], function (avalon) {
                 if (vm.currentPage < vm.totalPage) {
                     vm.currentPage++;
                     vm.getPage(vm.currentPage);
+                }
+            };
+            vm._getNextPage = function () {
+                if (vm.currentPage + 1 < vm.totalPage) {
+                    vm.getPage(vm.totalPage - 1);
                 }
             };
         },
