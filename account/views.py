82e4ffeffa3ac49533172676ef8c4a49ce59c634
lonelam <laizenan@gmail.com>
2017-07-09 01:40:37 +0800

    热身赛结束判断

--- account/views.py
+++ account/views.py
@@ -34,7 +34,7 @@ from .serializers import (UserLoginSerializer, UserRegisterSerializer,
                           UserProfileSerializer, TwoFactorAuthCodeSerializer)
 
 from .decorators import super_admin_required
-
+from contest.models import Contest, CONTEST_ENDED
 
 class UserLoginAPIView(APIView):
     def post(self, request):
@@ -75,8 +75,13 @@ def logout(request):
 
 
 def index_page(request):
+    warmup = Contest.objects.get(id=26);
+    cid = 26
+    if warmup.status == CONTEST_ENDED:
+        cid = 27
+    return render(request, "oj/index.html", {"cid": cid})
     if not request.user.is_authenticated():
-        return render(request, "oj/index.html")
+        return render(request, "oj/index.html", {"cid": 27})
 
     if request.META.get('HTTP_REFERER') or request.GET.get("index"):
         return render(request, "oj/index.html")
