9ace8e6a17d16e256a066f210803509f92cd1604
Lonelam <laizenan@gmail.com>
2017-09-04 17:16:17 +0800

    superadmin可见全部比赛

--- contest/views.py
+++ contest/views.py
@@ -559,7 +559,7 @@ def contest_list_page(request, page=1):
     # 正常情况
     contests = None
     if request.user.admin_type == SUPER_ADMIN:
-        constests = Contest.objects.filter().order_by("-create_time")
+        constests = Contest.objects.all().order_by("-create_time")
     else:
         constests = Contest.objects.filter(visible=True).order_by("-create_time")
 
